a225361755c8f65ea00731e9ac6ed821b8157758
OneLoveFaith <oneloveshinobu@gmail.com>
2022-11-27 03:25:01 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+(() => {
+	'use strict';
+  
+	const forms = document.querySelectorAll('.needs-validation');
+  
+	Array.from(forms).forEach(form => {
+	  form.addEventListener('submit', event => {
+		if (!form.checkValidity()) {
+		  event.preventDefault();
+		  event.stopPropagation();
+		}
+  
+		form.classList.add('was-validated');
+	  }, false);
+	});
+})();
+
+
+jQuery(document).ready(function( $ ) {
+
+	if($('#canvas').length) {
+
+		var doughnutData = [{
+        value: 70,
+        color: "#f85c37"
+      },
+      {
+        value: 30,
+        color: "#ecf0f1"
+      }
+    ];
+    var myDoughnut = new Chart(document.getElementById("canvas").getContext("2d")).Doughnut(doughnutData);
+	}
+
+	if($('#canvas2').length) {
+		var doughnutData = [{
+				value: 90,
+				color: "#f85c37"
+			},
+			{
+				value: 10,
+				color: "#ecf0f1"
+			}
+		];
+		var myDoughnut = new Chart(document.getElementById("canvas2").getContext("2d")).Doughnut(doughnutData);
+	}
+
+	if($('#canvas3').length) {
+		var doughnutData = [{
+				value: 55,
+				color: "#f85c37"
+			},
+			{
+				value: 45,
+				color: "#ecf0f1"
+			}
+		];
+		var myDoughnut = new Chart(document.getElementById("canvas3").getContext("2d")).Doughnut(doughnutData);
+	}
+
+	if($('#canvas4').length) {
+		var doughnutData = [{
+				value: 55,
+				color: "#f85c37"
+			},
+			{
+				value: 45,
+				color: "#ecf0f1"
+			}
+		];
+		var myDoughnut = new Chart(document.getElementById("canvas4").getContext("2d")).Doughnut(doughnutData);
+	}
+});
